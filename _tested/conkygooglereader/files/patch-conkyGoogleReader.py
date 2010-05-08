83ee4194f0b02076dcf9292bf9c03f15ee9ca5a0
User <zloidemon@zlobook.local>
2010-05-09 01:55:05 +0800

    new port: deskutils/conkygooglereader

---
+++ _tested/conkygooglereader/files/patch-conkyGoogleReader.py
@@ -0,0 +1,15 @@
+--- conkyGoogleReader.py.orig	2009-02-27 18:28:05.000000000 +0700
++++ conkyGoogleReader.py	2010-05-09 01:50:04.000000000 +0800
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python
++#!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+ ###############################################################################
+ # conkyEmail.py is a simple python script to gather 
+@@ -226,4 +226,4 @@
+ 
+ if __name__ == '__main__':
+     main()
+-    sys.exit()
+\ No newline at end of file
++    sys.exit()
