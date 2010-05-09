82aa9bea97fab2fa7c5d145c8cd2cce8b7fda576
User <zloidemon@zlobook.local>
2010-05-10 04:15:30 +0800

    added fixes

---
+++ _tested/feelhome/files/patch-client-web-controller-connection_controller.cpp
@@ -0,0 +1,11 @@
+--- client/web/controller/connection_controller.cpp.orig	2010-05-02 18:14:52.000000000 +0800
++++ client/web/controller/connection_controller.cpp	2010-05-09 22:35:17.000000000 +0800
+@@ -33,8 +33,6 @@
+     #elif defined __APPLE__
+     slash = QString("/");
+     os = QString(MACOSX);
+-    #elif defined (__FreeBSD__)
+-    xmlDocument.append(FREEBSD);
+     #else
+     slash = QString("/");
+     os = QString(LINUX);
---
+++ _tested/feelhome/files/patch-client-web-web_xml_creator.cpp
@@ -0,0 +1,20 @@
+--- client/web/web_xml_creator.cpp.orig	2010-02-18 00:01:58.000000000 +0700
++++ client/web/web_xml_creator.cpp	2010-05-09 22:40:53.000000000 +0800
+@@ -56,8 +56,6 @@
+     xmlDocument.append(LINUX);
+     #elif defined __APPLE__
+     xmlDocument.append(MACOSX);
+-    #elif defined (__FreeBSD__)
+-    xmlDocument.append(FREEBSD);
+     #else
+     xmlDocument.append(LINUX);
+     #endif
+@@ -111,8 +109,6 @@
+     xmlDocument.append(LINUX);
+     #elif defined __APPLE__
+     xmlDocument.append(MACOSX);
+-    #elif defined (__FreeBSD__)
+-    xmlDocument.append(FREEBSD);
+     #else
+     xmlDocument.append(LINUX);
+     #endif
