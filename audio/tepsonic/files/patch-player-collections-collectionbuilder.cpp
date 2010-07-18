2eea5f820c45a51b6426add6f17e1a5a91235227
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2010-07-18 23:25:53 +0800

    new version

--- audio/tepsonic/files/patch-player-collections-collectionbuilder.cpp
+++ audio/tepsonic/files/patch-player-collections-collectionbuilder.cpp
@@ -1,5 +1,5 @@
---- player/collections/collectionbuilder.cpp.orig	2010-05-18 02:04:47.000000000 +0800
-+++ player/collections/collectionbuilder.cpp	2010-05-18 18:09:23.000000000 +0800
+--- player/collections/collectionbuilder.cpp.orig	2010-06-19 00:09:28.000000000 +0800
++++ player/collections/collectionbuilder.cpp	2010-07-18 23:11:04.000000000 +0800
 @@ -22,9 +22,9 @@
  #include "collectionmodel.h"
  #include "databasemanager.h"
