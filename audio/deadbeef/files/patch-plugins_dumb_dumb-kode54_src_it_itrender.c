7b1e2479f3e0d05dfcc05af73357e69758dd0a86
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2011-03-31 02:46:26 +0800

    new version znotes, improssive, cuberok

---
+++ audio/deadbeef/files/patch-plugins_dumb_dumb-kode54_src_it_itrender.c
@@ -0,0 +1,10 @@
+--- plugins/dumb/dumb-kode54/src/it/itrender.c.orig	2011-03-31 02:20:25.000000000 +0800
++++ plugins/dumb/dumb-kode54/src/it/itrender.c	2011-03-31 02:34:44.000000000 +0800
+@@ -3559,7 +3559,6 @@ static void process_playing(DUMB_IT_SIGR
+ }
+ 
+ #ifndef __linux__
+-static inline float log2(float x) {return (float)log(x)/(float)log(2.0f);}
+ #endif
+ 
+ static int delta_to_note(float delta, int base)
--- misc/xyzcmd/files/patch-setup.py
+++ misc/xyzcmd/files/patch-setup.py
@@ -1,13 +1,14 @@
---- setup.py.orig	2010-05-06 04:30:03.000000000 +0800
-+++ setup.py	2010-05-09 03:20:33.000000000 +0800
-@@ -49,13 +49,9 @@
-                 ],
+--- setup.py.orig	2010-09-14 06:05:58.000000000 +0800
++++ setup.py	2011-03-30 22:08:33.000000000 +0800
+@@ -50,14 +50,9 @@ setup(
      data_files = [
          ("share/xyzcmd/conf", glob.glob("conf/*")),
--        ("share/doc/xyzcmd/api", glob.glob("doc/api/*")),
          ("share/xyzcmd/skins", glob.glob("skins/*")),
+-        ("share/doc/xyzcmd/api", glob.glob("doc/api/*")),
 -        ("share/doc/xyzcmd", ["ChangeLog", "doc/overview.pdf"]),
+         ("share/man/man1", ["doc/xyzcmd.1"])
          ] +
+-    include_rec("locale", "share/xyzcmd/%s") +
 -    include_rec("plugins", "share/xyzcmd/%s") +
 -    include_rec("doc/user-manual", "share/doc/xyzcmd/%s",
 -                lambda x: x.replace("doc/user-manual", "user-manual")),
