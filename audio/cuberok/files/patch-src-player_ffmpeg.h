be431f619e9c793d525db40c2297a4645cc4f0e0
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2011-03-30 20:53:30 +0800

    new version cueplayer, massexpert

--- audio/cuberok/files/patch-src-player_ffmpeg.h
+++ audio/cuberok/files/patch-src-player_ffmpeg.h
@@ -1,13 +1,13 @@
---- src/player_ffmpeg.h.orig	2009-07-10 14:14:03.000000000 +0800
-+++ src/player_ffmpeg.h	2010-05-10 21:49:51.000000000 +0800
-@@ -27,8 +27,8 @@
- #include <libavcodec/avcodec.h>
- #include <libavformat/avformat.h>
- #else
--#include <avcodec.h>
--#include <avformat.h>
-+#include <ffmpeg/avcodec.h>
-+#include <ffmpeg/avformat.h>
- #endif
+--- src/player_ffmpeg.h.orig	2011-03-30 18:37:08.000000000 +0800
++++ src/player_ffmpeg.h	2011-03-30 18:37:33.000000000 +0800
+@@ -23,8 +23,8 @@
+ extern "C" {
+ #define uint64_t UINT64_C
+ //#include <stdint.h>
+-#include <libavcodec/avcodec.h>
+-#include <libavformat/avformat.h>
++#include <avcodec.h>
++#include <avformat.h>
  }
- 
+ #include <QtCore>
+ #include "player.h"
