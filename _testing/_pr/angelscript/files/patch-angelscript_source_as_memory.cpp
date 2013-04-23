42596ff420ba423026ff67c03665138dae34fca4
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2013-04-23 11:58:28 +0000

    added new port angelscript

---
+++ _testing/_pr/angelscript/files/patch-angelscript_source_as_memory.cpp
@@ -0,0 +1,11 @@
+--- ../../source/as_memory.cpp.orig	2013-04-23 09:12:04.795967912 +0000
++++ ../../source/as_memory.cpp	2013-04-23 09:12:22.031967631 +0000
+@@ -38,7 +38,7 @@
+ 
+ #include <stdlib.h>
+ 
+-#if !defined(__APPLE__) && !defined( __SNC__ ) && !defined( __ghs__ )
++#if !defined(__APPLE__) && !defined( __SNC__ ) && !defined( __ghs__ ) && !defined(__FreeBSD__)
+ #include <malloc.h>
+ #endif
+ 
