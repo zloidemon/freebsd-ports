c3b919e94cda3b48f85edb8d256d385123f5b57f
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2013-04-24 23:23:38 +0400

    added new port libag_c

---
+++ _testing/libag_c/files/patch-as_c.cpp
@@ -0,0 +1,11 @@
+--- ../../as_c.cpp.orig	2013-04-23 11:09:19.006966230 +0000
++++ ../../as_c.cpp	2013-04-23 11:12:08.197968082 +0000
+@@ -36,7 +36,7 @@
+ //
+ 
+ // Include the C++ interface header so we can call the proper methods
+-#include "../../angelscript/include/angelscript.h"
++#include <angelscript.h>
+ 
+ BEGIN_AS_NAMESPACE
+ 
